0b550b8176448e8c4e866bc42e47874c3438323f
JaiRaga <raga.j.santhosh@gmail.com>
2022-05-09 15:21:17 +0530

    :sparkles: implemented graphql query to retrieve pins from the backend

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,6 +1,38 @@
-import pins from "../assets/data/pins";
+import { useEffect, useState } from "react";
+import { useNhostClient } from "@nhost/react";
 import MasonryList from "../components/MasonryList";
+import { Alert } from "react-native";
+
+const GET_PIN_QUERY = `
+query MyQuery {
+  pins {
+    created_at
+    id
+    image
+    title
+    user_id
+  }
+}
+`;
 
 export default function HomeScreen() {
+  const nhost = useNhostClient();
+
+  const [pins, setPins] = useState([]);
+
+  const fetchPins = async () => {
+    const response = await nhost.graphql.request(GET_PIN_QUERY);
+
+    if (response.error) {
+      Alert.alert("Error fetching pins");
+    } else {
+      setPins(response.data.pins);
+    }
+  };
+
+  useEffect(() => {
+    fetchPins();
+  }, []);
+
   return <MasonryList pins={pins} />;
 }
